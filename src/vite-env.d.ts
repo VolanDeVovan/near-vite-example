ff9181d604686ef223579ff37fc57f055f082b1b
Vladimir Kravchenko <volandevovan2004@gmail.com>
2022-05-07 15:50:25 +0300

    Add near-api-js

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+  // more env variables...
+  readonly VITE_CONTRACT_NAME: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Get it from neardev (cmd-shell) if not declared in .env
+if (!process.env.VITE_CONTRACT_NAME) {
+  process.env.VITE_CONTRACT_NAME = process.env.CONTRACT_NAME
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()]
